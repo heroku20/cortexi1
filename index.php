f081639a737209314432261bc40f487ed4a881b2
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-18 13:06:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,244 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cortexi - All Natural Product</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image" href="images/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="https://bestofferhere.link/Cortexiofficial"></script>
+
+</head>
+
+<body>
+    <header>
+        <div class="container flx">
+
+            <div class="brand">
+                <h1>Cortexi</h1>
+                <!--<img src="images/xyz-logo.png">--->
+            </div>
+            <nav><img src="images/bars-solid.svg" width="20" class="icon">
+                <ul>
+                    <li><a href="https://bestofferhere.link/Cortexiofficial">Home</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Terms of Use</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!---------Home--------------->
+    <div class="home">
+
+        <div class="container">
+            <div class="home-des-torc pqr">
+                <div class="home-img">
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/PRODx6-500px.webp" width="400"></a>
+                </div>
+                <div class="home-content-torc">
+                    <a href="https://bestofferhere.link/Cortexiofficial"
+                        style="color:#ff0000">
+                        <h2>*96% Of Customers Order 6 Bottles (Our Recommended Option)</h2>
+                    </a>
+                    <h3>360 Degree Hearing Support</h3>
+                    <h3>Cortexi the Herbal Science Breakthrough in Hearing Health</h3>
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/addtocart.png" width="400" ></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-----------------About---->
+    <div class="about">
+        <div class="container">
+            <div class="about-head-torc">
+                <h2>360° Hearing Support Give Your Ears A Rest</h2>
+                <p><b>Try out the new & improved Cortexi formula</b></p>
+            </div>
+            <div class="about-bottom-torc pqr">
+
+                <div class="about-content-torc">
+                    <p>When I first started working on the formula that was to become Cortexi, I never would have imagined in a million years that I would one day help thousands of people from around the world fulfill their dreams of improving their hearing
+                        health.</p>
+
+                    <p>It’s people like you that make all those years of research and testing worthwhile, and I hope you manage to get the most out of Cortexi while I can still afford to produce it at these prices.</p>
+
+                    <ul>
+                        <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> HEALTHY HEARING SUPPORT</li>
+                        <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> NATURAL MEMORY SHIELDING</li>
+                        <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> MENTAL SHARPNESS AID</li>
+                        <li><i class="fa fa-check-circle-o" aria-hidden="true"></i> OVERALL INFLAMMATION REDUCTION</li>
+                    </ul>
+                    <div class="box-button">
+                        <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/addtocart.png" width="500"></a>
+                    </div>
+                </div>
+                <div class="about-img"><a href="https://bestofferhere.link/Cortexiofficial"><img src="images/PRODx1-badge.webp" width="400"></a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!----price
+        <div class="price">
+            <div class="container">
+                
+            </div>
+        </div>--->
+
+    <!-----work------>
+    <div class="work" style="padding-top:20px;">
+        <div class="container">
+            <div class="work-content">
+                <h2>That\'s why they created...</h2>
+                <p>Are you worried about damaging your hearing? Have you experienced any mental fog or a decrease in your memory as of late? Is the thought of having to rely on a hearing aid frightening to you?</p>
+                <p>Well, I’ve got good news for you. You can take steps to protect your ears from damage starting today. their new and improved Cortexi formula works to shield your ears using only research-backed, all-natural ingredients combined in the
+                    precise ratios required to support healthy hearing.</p>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="benefits">
+        <div class="container">
+            <h2>Advantages Of Cortexi Supplement</span>
+            </h2>
+            <p style="text-align:center;">As our body does not hold the damage caused by the sugar crisis. Therefore, this unique supplement is designed to fight back and improve your health. </p>
+            <div class="benifits1 pqr">
+                <div class="benefits-img"><a href="https://bestofferhere.link/Cortexiofficial"><img src="images/benifits.webp" width="100%"></a>
+                    <div class="box-button">
+                        <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/addtocart.png" width="500"></a>
+                    </div>
+                </div>
+                <div class="benifit-main">
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>Healthy Hearing Support</p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>​Cortexi is 100% natural and side effect free</p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>Natural Memory Shielding</p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>This supplement reduce sugar levels alleviates brain fog.</p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>Cortexi is <b>OVERALL INFLAMMATION REDUCTION</b></p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>The supplement is manufactured in an FDA-registered and GMP-certified facility in the USA.</p>
+                        </div>
+                    </div>
+                    <div class="benifit-content-torc">
+                        <div class="igh"><i class="fa fa-check-circle-o" aria-hidden="true"></i></div>
+                        <div class="wdh">
+                            <p>​​For each purchase, you will get a 60-day of 100% money-back guarantee.</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="boxes">
+        <div class="container">
+            <div class="box-heading">
+                <h3>Cortexi</h3>
+                <a href="https://bestofferhere.link/Cortexiofficial"
+                    style="color:#ff0000;">
+                    <p><u>Claim Your Discounted Cortexi Below While Stock Lasts</u></p>
+                </a>
+            </div>
+            <div class="box-content">
+                <div class="box">
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/1bottle-price.webp" width="350"></a>
+                </div>
+                <div class="box">
+
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/3bottle-price.webp" width="350"> </a>
+
+                </div>
+                <div class="box">
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/6bottle-price.webp" width="350"></a>
+
+                </div>
+            </div>
+            <div class="box-button">
+                <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/addtocart.png" width="500"></a>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="gaurantee" style="padding-top:20px;">
+        <div class="container">
+            <h2>ABOUT THE GUARANTEE 60-DAYS</h2>
+            <div class="ginfo pqr">
+                <div class="g-img1">
+                    <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/gaurantee.png" width="250" style="border-radius:50%"></a>
+                </div>
+                <div class="g-img2">
+                    <a href="https://bestofferhere.link/Cortexiofficial"> <img src="images/g1.png" width="800"></a>
+                </div>
+            </div>
+            <h3>60-DAY MONEY BACK GUARANTEE 100% SATISFACTION GUARANTEED</h3>
+            <p>Cortexi comes with a 100% money back guarantee - 60 full days from your original purchase. If you\'re not totally and completely satisfied with their product or your results within the first 60 days simply let them know by calling their toll-free
+                number or dropping them an email and they\'ll gladly give you a full refund within 48 hours of the product being returned. That\'s right - simply return the product, even your empty bottles, anytime within 60 days of your purchase and you\'ll
+                receive a full, no-questions-asked refund (minus shipping and handling fees).</p>
+            <div class="box-button">
+                <a href="https://bestofferhere.link/Cortexiofficial"><img src="images/addtocart.png" width="500"></a>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="footer">
+        <div class="container">
+            <div class="footer-link-torc">
+                <p>&copy; 2023 Cortexi. All rights Reserved</p>
+                <ul>
+                    <li><a href="">Terms</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Contact Us</a></li>
+
+                </ul>
+                <small style="color:#fff;padding-bottom:10px;">Company is an independent service that is supported by advertising. The offers that appear on this article are from companies which they receives compensation. Editorial Note: The editorial content on this page is created by and property of them. It is not provided by the companies whose products and service are discussed. Opinions expressed here are okinawasite\'s alone and have not been reviewed, approved or otherwise endorsed by any third party.</small>
+            </div>
+        </div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $(".icon").click(function() {
+                $("nav ul").slideToggle();
+            });
+        });
+    </script>
+</body>
+
+</html>
+';
+?>
